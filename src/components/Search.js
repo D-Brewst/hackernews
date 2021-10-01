df5b50248550454ba80ea0be88e9c112cbec69be
D-Brewst <devonbrewster31@gmail.com>
2021-10-01 19:31:47 -0400

    wired up hnapi and set up search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { searchHnApi } from '../utils/hnapi.js';
 
 const Search = () => {
+    const searchRef = useRef();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(searchRef.current.value);
+        const data = await searchHnApi(searchRef.current.value);
+        console.log(data);
+    }
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                aria-disabled="false"
+                ref={searchRef}
+                placeholder="Search"
+                />
+                <div>
+                    <button className="login__btn" type="submit">Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
